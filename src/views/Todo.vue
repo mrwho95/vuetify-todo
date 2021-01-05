20fd432be0b61e90324a48cc49a1becf998179cc
mrwho95 <dnkh903@gmail.com>
2021-01-06 07:10:09 +0800

    add header to app vue, add list to todo page

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-list flat class="pt-0">
+      <div 
+      v-for="task in tasks"
+      :key="task.id"
+      >
+        <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{task.title}}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up'
+        },
+        {
+          id: 2,
+          title: 'Hello'
+        },
+        {
+          id: 3,
+          title: 'Hi'
+        },
+      ]
+    }
   }
 }
 </script>
