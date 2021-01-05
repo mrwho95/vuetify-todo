d8fd1a3dae8aa62a62e5c6d76e37588db0511a26
mrwho95 <dnkh903@gmail.com>
2021-01-06 07:48:10 +0800

    apply add and delete task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
     <v-list flat class="pt-0">
       <div 
       v-for="task in tasks"
       :key="task.id"
       >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{'blue lighten-5': task.done}"
+        >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
@@ -17,6 +31,15 @@
             <v-list-item-content>
               <v-list-item-title>{{task.title}}</v-list-item-title>
             </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn 
+              @click.stop="deleteTask(task.id)"
+              icon
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -30,20 +53,42 @@ export default {
   name: 'Home',
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
-        {
-          id: 1,
-          title: 'Wake up'
-        },
-        {
-          id: 2,
-          title: 'Hello'
-        },
-        {
-          id: 3,
-          title: 'Hi'
-        },
+        // {
+        //   id: 1,
+        //   title: 'Wake up',
+        //   done: false
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Hello',
+        //   done: false
+        // },
+        // {
+        //   id: 3,
+        //   title: 'Hi',
+        //   done: false
+        // },
       ]
+    }
+  },
+  methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id )
     }
   }
 }
